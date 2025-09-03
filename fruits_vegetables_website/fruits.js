48ce68e722ae7a8801029baac509afcbd1101aa4
Morenikejimy <amorenikeji198@gmail.com>
2025-09-03 05:50:43 -0700

    initial commit

---
+++ fruits_vegetables_website/fruits.js
@@ -0,0 +1,10 @@
+//  Swipper
+    var swiper = new Swiper(".home-swipper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+ 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
